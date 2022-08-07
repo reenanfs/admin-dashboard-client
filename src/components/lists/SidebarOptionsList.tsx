87fcd51135f7bc3d7f3f4f85c8dc5bf39c2e11f9
Renan Ferreira de Santana <renan.sant@ibm.com>
2022-08-07 20:15:11 -0300

    corrected casing on  install dependencies

--- src/components/lists/SidebarOptionsList.tsx
+++ src/components/lists/SidebarOptionsList.tsx
@@ -6,8 +6,6 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
-import { SvgIconTypeMap } from '@mui/material/SvgIcon';
 
 interface ISidebarOption {
   title: string;
